bd336065029e26fb7321e8de98601d6d902e9f9d
Muhammad Reyhan Abizar <reyhan.abizar@graveltechnology.com>
2024-09-24 15:01:50 +0700

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -35,6 +37,7 @@ export default function RootLayout({
           </div>
         </>
       </body>
+      <GoogleAnalytics gaId="G-G9RBZJFR8E" />
     </html>
   )
 }
